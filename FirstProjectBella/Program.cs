e7ca7fe63cb132e6d7436757bed8544d14ce8736
Snick <Snick@DESKTOP-SR38HOC>
2023-02-16 16:25:11 +0400

    Add project files.

---
+++ FirstProjectBella/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * This is my first step in C# world
+ */
+
+
+class MyMain
+{
+    static void Main(string[] args)
+    {
+        //Just greeting
+        Console.WriteLine("Welcome to C# Bella");
+        Console.WriteLine("Wish you all the best");
+        regularMethod();
+    }
+
+    /// This method does not show the subject as it is.
+    static void regularMethod()
+    {
+        Console.WriteLine();
+    }
+}
